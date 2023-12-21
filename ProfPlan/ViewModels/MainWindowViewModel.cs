1d28ad780484bda4d6083aab5741e7992a770108
Dmitrii <90130328+dmitriina1@users.noreply.github.com>
2023-12-21 19:21:24 +0500

    Тестирование работы модели представления

---
+++ ProfPlan/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProfPlan.ViewModels.Base;
+
+namespace ProfPlan.ViewModels
+{
+    internal class MainWindowViewModel: ViewModel
+    {
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
